573d587eae42254c1d22e0043badbda0915ebe28
Leticia Evelin <ljkeleticia@gmail.com>
2023-03-14 10:25:50 -0300

    primeira tela concluida

--- app/src/main/java/br/senai/sp/jandira/loginapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/loginapp/MainActivity.kt
@@ -1,105 +1,205 @@
 package br.senai.sp.jandira.loginapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.RememberObserver
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.loginapp.ui.theme.LOGINAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             LOGINAppTheme {
-                loginScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+
             }
         }
     }
 }
 
-@Preview(showSystemUi = true)
 @Composable
-fun loginScreen() {
+fun LoginScreen(){
 
-var emailState by rememberSaveable {
-    mutableStateOf(" ")
-}
+    val context = LocalContext.current
 
+    // Column principal
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween) {
 
-
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            //HEADER
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp),
-                horizontalAlignment = Alignment.Start
+        // Inicio da Row da forma
+        Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End) {
+            Card(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp),
+                shape = RoundedCornerShape(bottomStart = 16.dp),
+            backgroundColor = Color(207, 6, 240)
             ) {
-                Text(
-                    text = stringResource(id = R.string.title),
-                    fontSize = 42.sp,
-                    color = Color(207, 6, 240),
-                    fontWeight = FontWeight.ExtraBold,
-                    fontFamily = FontFamily.SansSerif
 
-                )
-                Text(
-                    text = stringResource(id = R.string.description),
-                    modifier = Modifier.padding(bottom = 5.dp),
-                    fontSize = 12.sp ,
-                    color = Color(160, 156, 156, 255)
-                )
             }
-            //FORM
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(24.dp)
+        } //Fim da row da forma
+
+        // Inicio da Column login
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+        ) {
+            Text(text = stringResource(id = R.string.login),
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(206, 1, 240)
+            )
+            Text(text = stringResource(id = R.string.please),
+            fontSize = 14.sp,
+                color = Color(160, 156, 156)
+            )
+        } // Fim da column login
+
+       // Inicio da column formulario
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                label = {
+                    Text(text = stringResource(id = R.string.email))
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(
+                        id = R.drawable.email_24),
+                        contentDescription = stringResource(
+                            id = R.string.email_description
+
+                        ),
+                        tint = Color(207, 1, 240)
+                    )
+                }
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                label = {
+                    Text(text = stringResource(id = R.string.password))
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(
+                            id = R.drawable.lock_24),
+                        contentDescription = stringResource(
+                            id = R.string.email_description
+
+                        ),
+                        tint = Color(207, 1, 240)
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color(207, 1, 240)),
+                shape = RoundedCornerShape(16.dp)
             ) {
-                Text(
-                    text = stringResource(id = R.string.email_label),
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                OutlinedTextField(
-                    value = emailState, //variavel de estado
-                    onValueChange = {
-                        Log.i("ds2m", it)
-                        emailState = it
-                    },
-                    modifier = Modifier.fillMaxWidth(),
-                    shape = RoundedCornerShape(16.dp),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-                )
+                Row() {
 
+                    Text(text = stringResource(id = R.string.button_sign).uppercase(),
+                        fontSize = 16.sp,
+                        fontWeight =  FontWeight.ExtraBold,
+                        color = Color.White
+                    )
+                    Icon(
+                       painter = painterResource
+                           (id = R.drawable.arrow_forward_24),
+                       contentDescription = " ",
+                       tint = Color.White
+                   )
+                }
 
+            }
 
+        } //Fim da column formulario
 
+        //Inicio da row don't have an account
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            horizontalArrangement = Arrangement.End) {
+
+            Text(
+                text = stringResource(id = R.string.question_account),
+                fontSize = 14.sp,
+                color = Color(160, 156, 156)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(id = R.string.signup),
+                modifier = Modifier.clickable {
+                  val intent = Intent(context, SignUpctivity::class.java)
+                   context.startActivity(intent)
+                },
+                fontSize = 14.sp,
+                color = Color(247, 6, 246)
+            )
+        } //Fim da row don't have an account
+
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start) {
+            Card(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp),
+                shape = RoundedCornerShape(topEnd = 16.dp),
+                backgroundColor = Color(207, 6, 240)
+            ) {
+
+            }
         }
-    }
+
+    } // Fim da column principal
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+     LOGINAppTheme {
+         LoginScreen()
+     }
+}
+
+
+
+
+  
